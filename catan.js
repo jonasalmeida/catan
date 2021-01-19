19342c16b8e15c46ecb519789162762fc56dca1a
Jonas Almeida <jalmeida@mathbiol.org>
2021-01-18 20:17:11 -0500

    empty early  plot

--- catan.js
+++ catan.js
@@ -2,35 +2,6 @@ catan={}
 catan.div=document.getElementById('catanCalc')
 catan.vals=[]
 
-if(catan.div){
-    let h='<div id="diceRes"></div>'
-    h+='<div id="dicePlot" width="500px">'
-    h+='<a href="https://github.com/jonasalmeida/catan" target="_bank">code</a>'
-    catan.div.innerHTML=h
-    // create dice result table
-    let div = document.getElementById('diceRes')
-    const ii = [...Array(12)]
-    ii.forEach((_,i)=>{
-        let j=i+2
-        let dv = document.createElement('div')
-        div.appendChild(dv)
-        dv.innerHTML=j>12? 'undo' : j
-        dv.id=`val_${j}`
-        dv.onclick=(ev)=>{
-            catan.vals.push(j)
-            console.log(catan.vals)
-            catan.plot()
-        }
-        if(dv.textContent=='undo'){
-            dv.onclick=(ev)=>{
-                catan.vals.pop()
-                console.log(catan.vals)
-                catan.plot()
-            }
-        }
-    })
-}
-
 catan.plot=(id="dicePlot")=>{
     /*
     let trace={
@@ -68,4 +39,34 @@ catan.plot=(id="dicePlot")=>{
         }
     }
     Plotly.newPlot(document.getElementById(id),[trace],layout,{displayModeBar: false})
+}
+
+if(catan.div){
+    let h='<div id="diceRes"></div>'
+    h+='<div id="dicePlot" width="500px">'
+    h+='<a href="https://github.com/jonasalmeida/catan" target="_bank">code</a>'
+    catan.div.innerHTML=h
+    // create dice result table
+    let div = document.getElementById('diceRes')
+    const ii = [...Array(12)]
+    ii.forEach((_,i)=>{
+        let j=i+2
+        let dv = document.createElement('div')
+        div.appendChild(dv)
+        dv.innerHTML=j>12? 'undo' : j
+        dv.id=`val_${j}`
+        dv.onclick=(ev)=>{
+            catan.vals.push(j)
+            console.log(catan.vals)
+            catan.plot()
+        }
+        if(dv.textContent=='undo'){
+            dv.onclick=(ev)=>{
+                catan.vals.pop()
+                console.log(catan.vals)
+                catan.plot()
+            }
+        }
+    })
+    catan.plot()
 }
